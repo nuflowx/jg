031dbfd0f444dd2bc64d082c4c2e3b2d89c38670
nuflowx <nuflowx@nulike.com>
2016-04-30 22:33:49 +0100

    set settings for building the site

--- config.rb
+++ config.rb
@@ -9,14 +9,19 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
 # General configuration
+
+# for nice looking urls without .html
+activate :directory_indexes
+
+# relative paths
+activate :relative_assets
+set :relative_links, true
+
+# assets to reload after changes
+activate :asset_hash
+
+Time.zone = 'London'
 
 ###
 # Helpers
@@ -32,7 +37,7 @@ page '/*.txt', layout: false
 # Build-specific configuration
 configure :build do
   # Minify CSS on build
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
   # activate :minify_javascript
